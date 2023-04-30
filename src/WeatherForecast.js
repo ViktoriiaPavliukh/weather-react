42aa5e50515079fd2d14964513e8930df0958306
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-04-30 16:24:40 +0100

    refactor: updated css styles for components

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
@@ -6,9 +6,22 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = "ed0417bf8fecd4ab27286ed64422cb0b";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (loaded) {
@@ -16,24 +29,21 @@ export default function WeatherForecast(props) {
       <div className="weather-forecast">
           <div className="row">
             {forecast.map(function(dailyForecast, index) {
-              if(index < 6) {
+              if(index < 5) {
                 return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast}/>
                 </div>
-                )
+                );
+              } else {
+                return null;
               }
             })}
           </div>
         </div>
    )
   } else {   
-    let apiKey = "ed0417bf8fecd4ab27286ed64422cb0b";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    load();
     return null;
   }
 }
